04a352b848df303a6d9f678e1481b3522c8065bf
Thong muan <punch025522552@gmail.com>
2025-03-28 14:32:05 +0700

    Add Home Page

---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:school_day/styles/styles.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'หน้าแรก',
+          style: textTheme.bodyMedium!.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: false,
+      ),
+      backgroundColor: backgroundColor,
+      body: const Center(),
+    );
+  }
+}
