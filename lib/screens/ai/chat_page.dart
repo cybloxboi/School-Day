8fe7d82115b331f0d8e339dbe073a77fc346da2e
cyblox <sukonlanatt@icloud.com>
2025-06-19 20:19:55 +0700

    feat: Update ChatPage to show maintenance message in debug mode and improve layout structure

--- lib/screens/ai/chat_page.dart
+++ lib/screens/ai/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:lottie/lottie.dart';
@@ -102,100 +103,103 @@ class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(32),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: _userInput == null
-                        ? Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            spacing: 16,
-                            children: [
-                              Text(
-                                'สวัสดี!',
-                                textAlign: TextAlign.center,
-                                style: textTheme.bodyMedium!.copyWith(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
+            child: !kDebugMode
+                ? Text('ขออภัย ตอนนี้กำลังอยู่ช่วงปิดปรับปรุง')
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: _userInput == null
+                              ? Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  spacing: 16,
+                                  children: [
+                                    Text(
+                                      'สวัสดี!',
+                                      textAlign: TextAlign.center,
+                                      style: textTheme.bodyMedium!.copyWith(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      'มีอะไรให้ช่วยไหม?',
+                                      style: textTheme.bodyMedium!.copyWith(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    LottieBuilder.asset(
+                                      'assets/animations/ai_introduction.json',
+                                      frameRate: FrameRate.max,
+                                      width: 300,
+                                      height: 300,
+                                    ),
+                                  ],
+                                )
+                              : AiChat(
+                                  userInput: _userInput,
+                                  geminiResponse: _geminiResponse,
+                                  promptAnimation: _promptAnimation,
+                                  userEmail: _currentUser!.email!,
                                 ),
-                              ),
-                              Text(
-                                'มีอะไรให้ช่วยไหม?',
-                                style: textTheme.bodyMedium!.copyWith(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              LottieBuilder.asset(
-                                'assets/animations/ai_introduction.json',
-                                frameRate: FrameRate.max,
-                                width: 300,
-                                height: 300,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      ConstrainedBox(
+                        constraints: const BoxConstraints(maxWidth: 700),
+                        child: Container(
+                          height: 55,
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(999),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withValues(alpha: 0.1),
+                                blurRadius: 10,
+                                offset: const Offset(0, 3),
                               ),
                             ],
-                          )
-                        : AiChat(
-                            userInput: _userInput,
-                            geminiResponse: _geminiResponse,
-                            promptAnimation: _promptAnimation,
-                            userEmail: _currentUser!.email!,
                           ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                ConstrainedBox(
-                  constraints: const BoxConstraints(maxWidth: 700),
-                  child: Container(
-                    height: 55,
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(999),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withValues(alpha: 0.1),
-                          blurRadius: 10,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(32, 8, 8, 8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Expanded(
-                            child: TextFormField(
-                              controller: _promptController,
-                              decoration: const InputDecoration.collapsed(
-                                hintText: 'ลองถาม AI ดูสิ',
-                              ),
-                              onEditingComplete: sendPrompt,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(32, 8, 8, 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Expanded(
+                                  child: TextFormField(
+                                    controller: _promptController,
+                                    decoration: const InputDecoration.collapsed(
+                                      hintText: 'ลองถาม AI ดูสิ',
+                                    ),
+                                    onEditingComplete: sendPrompt,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 16,
+                                ),
+                                IconButton.filledTonal(
+                                  onPressed: !_isButtonEnabled || _isLoading
+                                      ? null
+                                      : sendPrompt,
+                                  icon: _isLoading
+                                      ? LoadingAnimationWidget
+                                          .staggeredDotsWave(
+                                          color: Colors.grey,
+                                          size: 20,
+                                        )
+                                      : const Icon(Icons.send_rounded),
+                                ),
+                              ],
                             ),
                           ),
-                          const SizedBox(
-                            width: 16,
-                          ),
-                          IconButton.filledTonal(
-                            onPressed: !_isButtonEnabled || _isLoading
-                                ? null
-                                : sendPrompt,
-                            icon: _isLoading
-                                ? LoadingAnimationWidget.staggeredDotsWave(
-                                    color: Colors.grey,
-                                    size: 20,
-                                  )
-                                : const Icon(Icons.send_rounded),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ),
-              ],
-            ),
           ),
         ),
       ),
