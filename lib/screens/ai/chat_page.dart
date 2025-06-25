7b3f49b5d19bb52adbf9a062567a11105f93419e
cyblox <sukonlanatt@icloud.com>
2025-06-25 11:13:15 +0700

    refactor: Remove unused import from chat_page.dart

--- lib/screens/ai/chat_page.dart
+++ lib/screens/ai/chat_page.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:lottie/lottie.dart';
